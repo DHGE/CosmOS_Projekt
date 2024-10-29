80152d2f7212fe8ada7e40208a510e52ac34c5df
Nicrass <nicrassgw@gmail.com>
2024-10-29 13:03:15 +0100

    Added Users. Tried adding create-User Command
    TODO:
    add more checks for files (correct filenames, check if file already exists in Dir, check if dir already exists in dir)
    add more features and checks to create-User-Command (isEmpty, ifUsernameAlreadyExists, valid password (length and symbols)
    add Users into a Config File

---
+++ CosmOS_Projekt/Userverwaltung/User.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmOS_Projekt.Userverwaltung
+{
+    internal class User
+    {
+        private string username;
+        private string password;
+        private string vorname;
+        private string nachname;
+        private string permission;
+
+        public User(string username, string vorname, string nachname, string password)
+        {
+            this.username = username;
+            this.vorname = vorname;
+            this.nachname = nachname;
+            this.password = password;
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        public string Vorname
+        {
+            get { return vorname; }
+            set { vorname = value; }
+        }
+
+        public string Nachname
+        {
+            get { return nachname; }
+            set { nachname = value; }
+        }
+    }
+}
